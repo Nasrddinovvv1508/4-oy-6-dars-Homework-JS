686377478b83c6d69597694cda28d5816113e648
unknown <najmiddinnasriddinov5@gmail.com>
2024-04-04 11:25:15 +0500

    second done

--- index.js
+++ index.js
@@ -9,3 +9,20 @@
 //     console.log(key);
 // }
 
+
+        // 2-MASALA
+// const student = {
+// 	name : "David Rayy",
+// 	class : "VI",
+// 	rollno : 12 
+// }
+
+// function deleteRollNo() {
+//     delete student.rollno;
+//     for (const key in student) {
+//         console.log(key);;
+//     }
+// }
+
+// deleteRollNo()
+
