6bf126d4f1b7610c23b3f0cc4573c292785af6be
unknown <najmiddinnasriddinov5@gmail.com>
2024-04-04 11:24:36 +0500

    first done

---
+++ index.js
@@ -0,0 +1,11 @@
+        // 1-MASALA
+// const student = {
+//     name: "David Rayy",
+//     sclass: "VI",
+//     rollno: 12
+// }
+
+// for (const key in student) {
+//     console.log(key);
+// }
+
