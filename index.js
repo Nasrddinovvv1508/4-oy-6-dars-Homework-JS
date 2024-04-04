5a408ec3f0db2aa2125fc22830732ec980181797
unknown <najmiddinnasriddinov5@gmail.com>
2024-04-04 22:38:31 +0500

    All Done

--- index.js
+++ index.js
@@ -86,11 +86,13 @@
 
 
         // 6-MASALA
-// let numbers = {
-//         0: 10,
+// let myObject = {};
+
+// let keys = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]; 
+// let value = ["0"];
+
+// for (let i = 0; i < keys.length; i++) {
+//     myObject[keys[i]] = value++;
 // }
 
-// for (let key in numbers) {
-
-// }
-// console.log(numbers);
+// console.log(myObject);
